5d7604586696ce42f43e92bb24a2a7ed0a217b7c
aparappi <aparappi@iu.edu>
2018-12-07 12:31:06 -0500

    Modularized structure for each model. Added gitignore

--- orient.py
+++ orient.py
@@ -8,6 +8,10 @@ import sys
 import re
 import math
 from collections import defaultdict
+import numpy as np
+from adaboost import *
+from knn import *
+from forest import *
 
 # For testing purposes..remove below line for actual program
 sys.argv = ['program_name','train','train-data.txt','model_file.txt', 'adaboost']
@@ -22,4 +26,14 @@ model_file = sys.argv[3]
 with open(input_file, 'r', encoding="utf-8") as file:
         raw_input = file.read()
 
-        
+# Based on the model required, call respective functions
+if model == 'adaboost':
+    print("Adaboost")
+
+elif model == 'nearest':
+    print('K Nearest Neighbors model')
+
+elif model == 'forest':
+    print('Decision Trees')
+
+
