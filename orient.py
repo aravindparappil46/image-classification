2ce41339cd343cbc9a61fcb119bdd98ecffc6968
aparappi <aparappi@iu.edu>
2018-12-07 12:06:02 -0500

    Added sys args

---
+++ orient.py
@@ -0,0 +1,21 @@
+"""
+------------------------------------------------------------------------------------------
+Elements of AI | Assignment 4 | mkpandey-aparappi-atarfe
+------------------------------------------------------------------------------------------
+"""
+
+import sys
+import re
+import math
+from collections import defaultdict
+
+# For testing purposes..remove below line for actual program
+sys.argv = ['program_name','train','train_file.txt','model_file.txt', 'adaboost']
+
+# Fetching cmd-line args
+model = sys.argv[4]
+train_or_test = sys.argv[1]
+input_file = sys.argv[2]
+model_file = sys.argv[3]
+
+
