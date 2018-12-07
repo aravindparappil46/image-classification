d611a660277d4e1bc5659ec43a0da2567057c238
aparappi <aparappi@iu.edu>
2018-12-07 12:10:14 -0500

    Fixed file name for train-data

--- orient.py
+++ orient.py
@@ -10,7 +10,7 @@ import math
 from collections import defaultdict
 
 # For testing purposes..remove below line for actual program
-sys.argv = ['program_name','train','train_file.txt','model_file.txt', 'adaboost']
+sys.argv = ['program_name','train','train-data.txt','model_file.txt', 'adaboost']
 
 # Fetching cmd-line args
 model = sys.argv[4]
@@ -18,6 +18,8 @@ train_or_test = sys.argv[1]
 input_file = sys.argv[2]
 model_file = sys.argv[3]
 
-# Read file into a variable
+# Read input file into a variable
 with open(input_file, 'r', encoding="utf-8") as file:
         raw_input = file.read()
+
+        
