9ce80ea75e8d3484f5bc765d41bba884a7b55117
aparappi <aparappi@iu.edu>
2018-12-07 20:30:53 -0500

    Testing for adaboost

--- adaboost.py
+++ adaboost.py
@@ -4,10 +4,12 @@ Author: Aravind Parappil
 """
 import numpy as np
 import math
+import pickle as pickle
 from collections import defaultdict
+import operator
 
-def train(data, train_orientation, random_hyp_pairs, max_iterations):
-    hyp_alphas = defaultdict(dict)
+def train(data, train_orientation, random_hyp_pairs, max_iterations, f, hyp_alphas):
+    
     weights = np.array([[float(1/len(data))]]* len(data))
     my_labels = np.array([[-1]]* len(data))
        
@@ -24,7 +26,7 @@ def train(data, train_orientation, random_hyp_pairs, max_iterations):
             else:
                 my_labels[j] = -1
 
-            # first col of data contains actual labels. Comparing....
+            # First col of data contains actual labels. Comparing....
             # if mismatch, then update error as the sum of weight at row mismatched
             if my_labels[j] !=  data[:,0][j]:
                 error += weights[j]
@@ -37,12 +39,59 @@ def train(data, train_orientation, random_hyp_pairs, max_iterations):
 
         weights = normalize(weights)
         hyp_alphas[train_orientation][(column_1, column_2)] = math.log((1-error)/error)
-        
+    
     return hyp_alphas
 
 # Divide each element with sum of array
 def normalize(weights):
     total_sum = weights.sum()
     return np.divide(weights, total_sum)
-            
+
+
+# Testing
+def test(data, hyp_alphas, image_names):
+    f = open('output.txt', 'a')
+    correct_prediction_count = 0
+    
+    for file_index in range(len(data)):
+        predicted_orientation_dict = {}
+        for k,v in hyp_alphas.items():
+            summed_up = 0
+            for hyp_tuple, alpha in v.items():
+                diff = data[file_index][hyp_tuple[0]] - data[file_index][hyp_tuple[1]]
+                prod = diff * alpha
+                summed_up += prod
+            predicted_orientation_dict[k]= summed_up
+
+        # Finding which orientation has the largest value
+        # REFERENCE: https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+        final_prediction = str(max(predicted_orientation_dict.items(), key=operator.itemgetter(1))[0])
+        f.write(image_names[file_index]+' '+ final_prediction+'\n')
+
+        actual_orient = data[file_index][1]
+        if int(final_prediction) == actual_orient:
+            correct_prediction_count += 1
+
+    # Finding how many records were correctly identified
+    print((correct_prediction_count/len(data))*100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
--- orient.py
+++ orient.py
@@ -9,13 +9,14 @@ import re
 import math
 from collections import defaultdict
 import random
+import pickle as pickle
 import numpy as np
 import adaboost as adaboost
 from knn import *
 from forest import *
 
 # For testing purposes..remove below line for actual program
-sys.argv = ['program_name','train','train-data.txt','model_file.txt', 'adaboost']
+sys.argv = ['program_name','test','test-data.txt','model-file.pickle', 'adaboost']
 
 # Fetching cmd-line args
 train_or_test = sys.argv[1]
@@ -26,6 +27,9 @@ model = sys.argv[4]
 # Read input file into a variable..skips first column with image name
 data = np.loadtxt(input_file, usecols=range(1,194))
 
+# All image names stored in separate array
+image_names = np.genfromtxt(input_file, dtype="str", usecols=range(0,1))
+
 # Based on the model required, call respective functions
 if model == 'best':
     model = 'nearest'
@@ -35,39 +39,55 @@ if model == 'adaboost':
     #----------------------------------#
     #            TRAINING              #
     #----------------------------------#
-    random_hyp_pairs = []
-    max_iterations = 20
-    
-    # Finding total number of cols in data...Not counting the col with actual labels
-    # Since np array lengths are calculated from index 1, have to decrement 1 too
-    total_num_of_cols = len(data[0])-2
-    
-    for i in range(0,max_iterations):
-        # Getting two random column indices. Will find difference between row vals
-        # of these two columns, which will be our hypothesis
-        # Idea credit: Shivam Thakur
-        column_1 = random.randint(1,total_num_of_cols)
-        column_2 = random.randint(1,total_num_of_cols)
+    if train_or_test == 'train':
+        random_hyp_pairs = []
+        hyp_alphas = defaultdict(dict)
+        max_iterations = 20
+        f = open(model_file, 'wb')
 
-        # Resolving conflict, if any
-        if column_1 == column_2:
-            if column_2 != total_num_of_cols:
-                column_2 += 1
-            else:
-                column_2 -= 1
-                
-        random_hyp_pairs.append((column_1,column_2))
+        # Finding total number of cols in data...Not counting the col with actual labels
+        # Since np array lengths are calculated from index 1, have to decrement 1 too
+        total_num_of_cols = len(data[0])-2
         
-    alphas_for_0 = adaboost.train(data, 0, random_hyp_pairs, max_iterations)
-    alphas_for_90 = adaboost.train(data, 90, random_hyp_pairs, max_iterations)
-    alphas_for_180 = adaboost.train(data, 180, random_hyp_pairs, max_iterations)
-    alphas_for_270 = adaboost.train(data, 270, random_hyp_pairs, max_iterations)
+        for i in range(0,max_iterations):
+            # Getting two random column indices. Will find difference between row vals
+            # of these two columns, which will be our hypothesis while training
+            # Idea credit: Shivam Thakur
+            column_1 = random.randint(1,total_num_of_cols)
+            column_2 = random.randint(1,total_num_of_cols)
 
-    
+            # Resolving conflict, if any. Shouldn't be the same
+            if column_1 == column_2:
+                if column_2 != total_num_of_cols:
+                    column_2 += 1
+                else:
+                    column_2 -= 1
+                    
+            random_hyp_pairs.append((column_1,column_2))
+
+        # Running train for the different orientations...
+        # Each run will return a dict of dicts with key as particular orientation
+        # Pipeline the dicts returned from one training stage to another till we finish.
+        # Output will be a dict of dict with 4 keys (0, 90, 180, 270)
+        
+        alphas_for_0 = adaboost.train(data, 0, random_hyp_pairs, max_iterations, f, hyp_alphas)
+        alphas_for_90 = adaboost.train(data, 90, random_hyp_pairs, max_iterations, f, alphas_for_0)
+        alphas_for_180 = adaboost.train(data, 180, random_hyp_pairs, max_iterations, f, alphas_for_90)
+        alphas_for_270 = adaboost.train(data, 270, random_hyp_pairs, max_iterations, f, alphas_for_180)
+
+        # Storing model params in a pickle to retain dictionary structure
+        pickle.dump(alphas_for_270, f, protocol=pickle.HIGHEST_PROTOCOL)
+        # Training ends....
+              
     #----------------------------------#
     #            TESTING               #
     #----------------------------------#
-
+    elif train_or_test == 'test':      
+        f = open(model_file, 'rb')
+        hyp_alphas = pickle.load(f)
+        adaboost.test(data, hyp_alphas, image_names)
+        
+        
     
 elif model == 'nearest':
     print('K Nearest Neighbors model')
