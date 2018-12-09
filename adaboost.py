4f8a721f9e029eee50568ed0518fced9c02c52eb
aparappi <aparappi@iu.edu>
2018-12-08 19:55:08 -0500

    Adaboost testing proper now

--- adaboost.py
+++ adaboost.py
@@ -8,39 +8,41 @@ import pickle as pickle
 from collections import defaultdict
 import operator
 
-def train(data, train_orientation, random_hyp_pairs, max_iterations, f, hyp_alphas):
-    
+def train(data, train_orientation, random_hyp_pairs, max_iterations, hyp_alphas):
+    print("Training for ", train_orientation)
     weights = np.array([[float(1/len(data))]]* len(data))
     my_labels = np.array([[-1]]* len(data))
        
-    for i in range(0, max_iterations):
-        error = 0
-        
+    for i in range(0, max_iterations):              
         column_1 = random_hyp_pairs[i][0]
         column_2 = random_hyp_pairs[i][1]
+        error = 0
         
         # Assigning labels based on hyp    
         for j in range(len(data)):
-            if data[j][column_1] - data[j][column_2] < 0:
+            if data[j][column_1] - data[j][column_2] > 0:
                 my_labels[j] = train_orientation
             else:
                 my_labels[j] = -1
 
             # First col of data contains actual labels. Comparing....
             # if mismatch, then update error as the sum of weight at row mismatched
-            if my_labels[j] !=  data[:,0][j]:
+            
+            if my_labels[j][0] !=  data[:,0][j]:
                 error += weights[j]
-
+   
         # Error value has been calculated now...
-        # Updating weights for correctly classified rows 
+        # Updating weights for correctly classified rows
+      
         for k in range(len(data)):
             if my_labels[k] == data[:,0][k]:
-                weights[k] *= (error/(1-error))
+                weights[k] *= (float((error/(1-error))))
 
         weights = normalize(weights)
-        hyp_alphas[train_orientation][(column_1, column_2)] = math.log((1-error)/error)
-    
+        hyp_alphas[train_orientation][(column_1, column_2)] = math.log(float((1-error)/error))
+            
     return hyp_alphas
+
 
 # Divide each element with sum of array
 def normalize(weights):
@@ -59,10 +61,13 @@ def test(data, hyp_alphas, image_names):
             summed_up = 0
             for hyp_tuple, alpha in v.items():
                 diff = data[file_index][hyp_tuple[0]] - data[file_index][hyp_tuple[1]]
-                prod = diff * alpha
+                if diff > 0:
+                    prod = 1 * alpha
+                else:
+                    prod = -1 * alpha
                 summed_up += prod
             predicted_orientation_dict[k]= summed_up
-
+        
         # Finding which orientation has the largest value
         # REFERENCE: https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
         final_prediction = str(max(predicted_orientation_dict.items(), key=operator.itemgetter(1))[0])
@@ -71,27 +76,6 @@ def test(data, hyp_alphas, image_names):
         actual_orient = data[file_index][1]
         if int(final_prediction) == actual_orient:
             correct_prediction_count += 1
-
+    
     # Finding how many records were correctly identified
     print((correct_prediction_count/len(data))*100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
--- orient.py
+++ orient.py
@@ -14,6 +14,7 @@ import numpy as np
 import adaboost as adaboost
 from knn import *
 from forest import *
+import datetime
 
 # For testing purposes..remove below line for actual program
 sys.argv = ['program_name','test','test-data.txt','model-file.pickle', 'adaboost']
@@ -42,9 +43,11 @@ if model == 'adaboost':
     if train_or_test == 'train':
         random_hyp_pairs = []
         hyp_alphas = defaultdict(dict)
-        max_iterations = 20
+        max_iterations = 1230
         f = open(model_file, 'wb')
 
+        start = datetime.datetime.now()
+        
         # Finding total number of cols in data...Not counting the col with actual labels
         # Since np array lengths are calculated from index 1, have to decrement 1 too
         total_num_of_cols = len(data[0])-2
@@ -65,20 +68,27 @@ if model == 'adaboost':
                     
             random_hyp_pairs.append((column_1,column_2))
 
+     
         # Running train for the different orientations...
         # Each run will return a dict of dicts with key as particular orientation
         # Pipeline the dicts returned from one training stage to another till we finish.
         # Output will be a dict of dict with 4 keys (0, 90, 180, 270)
-        
-        alphas_for_0 = adaboost.train(data, 0, random_hyp_pairs, max_iterations, f, hyp_alphas)
-        alphas_for_90 = adaboost.train(data, 90, random_hyp_pairs, max_iterations, f, alphas_for_0)
-        alphas_for_180 = adaboost.train(data, 180, random_hyp_pairs, max_iterations, f, alphas_for_90)
-        alphas_for_270 = adaboost.train(data, 270, random_hyp_pairs, max_iterations, f, alphas_for_180)
 
+        print("Max iterations set to: ", max_iterations)
+        alphas_for_0 = adaboost.train(data, 0, random_hyp_pairs, max_iterations, hyp_alphas)
+        alphas_for_90 = adaboost.train(data, 90, random_hyp_pairs, max_iterations, alphas_for_0)
+        alphas_for_180 = adaboost.train(data, 180, random_hyp_pairs, max_iterations, alphas_for_90)
+        alphas_for_270 = adaboost.train(data, 270, random_hyp_pairs, max_iterations, alphas_for_180)
+
+        
         # Storing model params in a pickle to retain dictionary structure
         pickle.dump(alphas_for_270, f, protocol=pickle.HIGHEST_PROTOCOL)
+        end = datetime.datetime.now()
+
+        print("Finished training in", end-start)
         # Training ends....
-              
+        f.close()
+        
     #----------------------------------#
     #            TESTING               #
     #----------------------------------#
@@ -86,7 +96,7 @@ if model == 'adaboost':
         f = open(model_file, 'rb')
         hyp_alphas = pickle.load(f)
         adaboost.test(data, hyp_alphas, image_names)
-        
+        f.close()
         
     
 elif model == 'nearest':
